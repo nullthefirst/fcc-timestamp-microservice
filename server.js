9afc9d1c3781f1fc6e6c0cfee670faa7256d769d
Ninte <ninte.dev@gmail.com>
2021-08-05 11:22:59 +0100

    Added first solution attempt.

--- server.js
+++ server.js
@@ -6,25 +6,32 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' });
 });
 
-
+// solutions
+app.get('/api/:date?', function (req, res) {
+  let responseObj;
+  if (Date.parse(req.params.date) !== NaN) {
+    responseObj['unix'] = Date.parse(`${req.params.date}`);
+    responseObj['utc'] = new Date(`${req.params.date}`);
+  }
+  res.json(responseObj);
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
